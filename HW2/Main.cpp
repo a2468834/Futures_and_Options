8242bb8a4ac3d85f4881d296a556148f1408ffe0
a2468834 <a2468834@gmail.com>
2018-12-11 03:37:17 +0800

    Fix tiny bugs about output format.

--- HW2/Main.cpp
+++ HW2/Main.cpp
@@ -14,13 +14,18 @@ int main()
 {
 	vector<BinoModel> BM;
 
+	cout<<"European Put"<<endl;
+	cout<<endl;
+
 	// Using binomial tree model
 	cout<<"Binomial Model:"<<endl;
 	for(double i=0.5; i<252.5; i=i+0.5)BM.emplace_back(0, 1, i, 50, 0.3, 52, 2, 0.05, 1, 0);
+	cout<<endl;
 	
 	// Using Black-Scholes model
 	cout<<"Black-Scholes Model:"<<endl;
 	BM.emplace_back(0, 1, 252, 50, 0.3, 52, 2, 0.05, 1, 1);
+	cout<<endl;
 
 	return 0; 
 }
